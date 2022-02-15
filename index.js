010f7fd15aa06b47f339c92298cad08ae0c0d156
Gokul S <34234110+gokulsg@users.noreply.github.com>
2022-02-15 01:55:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+$(function() {
+	var message = {
+
+		message: [
+			'Machine Learning enthusiast', 
+			'Masters student at Saarland University', 
+			'Budding Data Scientist', 
+      'AWS Community Builder',
+			'Professional Certified Neo4j Developer'
+		],
+		counterS: 0,
+		counterL: 0,
+		deleteS: false,
+
+		init: function() {
+			this.cacheElem();
+			this.type();
+		},
+
+		cacheElem: function() {
+			this.$text = $('.text');
+		},
+
+		type: function() {
+			var message 	= this.message[this.counterS],
+				  that 	    = this,
+				  speed 	  = 0;
+
+			message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
+			if(this.message[this.counterS] != message && !this.deleteS) {
+				this.$text.text(message);
+				speed = 90;
+			}
+			else {
+				this.deleteS = true;
+				speed = this.message[this.counterS] == message ? 1000 : 40;
+				this.$text.text(message);
+				if (message == '') {
+					this.deleteS = false;
+					this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
+				}
+			}
+			setTimeout(function(){that.type()}, speed);
+		}
+	};
+	message.init();
+});
